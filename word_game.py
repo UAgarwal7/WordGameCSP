483f771ad34c12df8e0512fb2bd237c15c49b7cd
UAgarwal7 <utsavagar7@gmail.com>
2023-04-11 11:47:12 -0700

    Started the code for the guess function. Facing problems with dictionaries rn

--- word_game.py
+++ word_game.py
@@ -21,12 +21,34 @@ word processing at the beginning:
     search for duplicates
         get to later - lower priority
 
-
-function guess (word to guess)
     input guessed word
     make each letter of the guessed word an element of a list guessed word
+function guess (word to guess list, guessed list)
     for loop going through the indexes of the guessed word list (iteration)
         use the find function in a try-except for each letter in the guessed string to get the index
         if it is successful, add the letter and the index to a dictionary (selection)
     Once the entire guessed word has been gone through, return the dictionary
+
+Edge cases to test:
+    duplicate letters in answer
+    duplicate letters in guess
 """
+answer_word = input("Enter the word to be guessed: ")
+answer_list = list(answer_word)
+# insert code to search for duplicate here
+current_guessed_letters = {}
+
+game_end = False
+while not game_end:
+    guessed_word = input("Guess a word: ")
+    guessed_list = list(guessed_word)
+    for x in range (0,len(guessed_list)):
+        current_letter: guessed_list[x]
+        try:
+            answer_index = answer_list.index(current_letter)
+        except:
+            break
+        else:
+            current_guessed_letters[current_letter] = answer_index - x
+            # gives the differences of the indexes of the letter in the guessed word and the answer
+        print(current_guessed_letters)
